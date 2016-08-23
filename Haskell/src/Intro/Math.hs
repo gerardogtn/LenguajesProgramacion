4440d1ed9223854e5fa1b5af21f1cb527dfdf08a
Gerardo Teruel <gerardogtn@gmail.com>
2016-08-22 23:14:45 -0500

    Add maximum iterations

--- Haskell/src/Intro/Math.hs
+++ Haskell/src/Intro/Math.hs
@@ -16,9 +16,13 @@ factorialStack 0 = 1
 factorialStack n = n * factorialStack (n - 1)
 
 newtonRaphson :: (Floating a, Ord a) => (a -> a) -> (a -> a) -> a -> a
-newtonRaphson f df x0
-              | abs (x1 - x0) < epsilon = x0
-              | otherwise = newtonRaphson f df x1
-              where
-                epsilon = 1/100
-                x1 = x0 - ( f x0 / df x0)
+newtonRaphson f df x0 = newtonRaphson' f df x0 0
+    where
+      newtonRaphson' f df x0 i
+        | i == maxIt = error "A solution couldn't be found"
+        | abs (x1 - x0) < epsilon = x0
+        | otherwise = newtonRaphson' f df x1 (i + 1)
+        where
+        maxIt = 50
+        epsilon = 1/100
+        x1 = x0 - ( f x0 / df x0)
