5784f4da3b87efa007c674527a4dc0df7f34af27
Gerardo Teruel <gerardogtn@gmail.com>
2016-09-06 16:55:08 -0500

    Optimize primes and isPrime

--- Haskell/src/Homework/Homework03.hs
+++ Haskell/src/Homework/Homework03.hs
@@ -1,8 +1,8 @@
 module Homework.Homework03
     ( smallestDivisor,
-      isDivisible,
       nthPrime,
       circularPrimes,
+      primes,
       isPrime
     ) where
 import Data.List
@@ -13,23 +13,36 @@ smallestDivisor lowerBound upperBound = smallestDivisor' [lowerBound..upperBound
           | null elements = acc
           | otherwise = smallestDivisor' (tail elements) (lcm (head elements) acc)
 
-isDivisible:: (Integral a) => [a] -> a -> Bool
-isDivisible elements num = all (\x -> num `mod` x == 0) elements
-
 nthPrime :: (Integral a) => Int -> a
 nthPrime n = getPrimes !! (n - 1)
 
 circularPrimes :: Integer -> [Integer]
-circularPrimes upperBound = filter isCircularPrime [1..upperBound]
+circularPrimes upperBound = filter isCircularPrime (primes upperBound)
 
 isCircularPrime :: Integer -> Bool
 isCircularPrime x = all isPrime (map (\y -> read y :: Integer) (permutations (show x)))
 
 -- highestPrimeSum
 
+primes :: Integer -> [Integer]
+primes n =
+  let primeApproximation = [2..n]
+  in primes' primeApproximation []
+  where
+    primes' [] acc = acc
+    primes' elements acc =
+          let nextPrimes = filter (\x -> x `mod` head elements /= 0) elements
+          in
+          primes' nextPrimes (acc++[head elements])
+
 getPrimes :: (Integral a) => [a]
 getPrimes = filter isPrime [1..]
 
 isPrime :: (Integral a) => a -> Bool
 isPrime 1 = False
-isPrime n = null [ x | x <- [2..n - 1], n `mod`x  == 0]
+isPrime 2 = True
+isPrime 3 = True
+isPrime n
+  | n `mod` 2 == 0 = False
+  | n `mod` 3 == 0 = False
+  | otherwise = null [ x | x <- [2..n - 1], n `mod`x  == 0]
