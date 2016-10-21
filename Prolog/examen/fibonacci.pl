2887b3ea29cc91c60c45cacb6bf34fafe621eb83
Gerardo Teruel <gerardogtn@gmail.com>
2016-10-21 15:00:28 -0500

    Add dynamic programming fibonacci

--- Prolog/examen/fibonacci.pl
+++ Prolog/examen/fibonacci.pl
@@ -2,17 +2,14 @@ fibonacci(0, 1).
 fibonacci(1, 1).
 fibonacci(N, Out) :- N > 1, Prev is N-1, Prev2 is N-2, fibonacci(Prev, A), fibonacci(Prev2, B), Out is A+B.
 
-
-fibonacciDp(N, Out) :- fibonacciDp(N, memo{0:1, 1:1}, Out).
-fibonacciDp(N, D, Out) :- Out is D.get(N), write(Out).
-fibonacciDp(N, memo, Out) :- N > 1,
+fibonacciDp(N, Out) :- asserta(known(0, 1)), asserta(known(1, 1)), known(N, Out).
+fibonacciDp(N, Out) :- N > 1,
                               Prev is N-1,
                               Prev2 is N-2,
-                              fibonacciDp(Prev, memo, A),
-                              fibonacciDp(Prev2, memo, B),
+                              fibonacciDp(Prev, A),
+                              fibonacciDp(Prev2, B),
                               Out is A+B,
-                              memo.put(N, Out),
-                              write(Out).
-
-
-testDict(D, K, V) :- D.put(K, V), write(D).
+                              asserta(known(N, Out)).
+nFibonaccis(0, [1]).
+nFibonaccis(1, [1, 1]).
+nFibonaccis(N, [H|T]) :- Next is N-1, fibonacciDp2(N, H), nFibonaccis(Next, T).
